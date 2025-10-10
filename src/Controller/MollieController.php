0a522da9178d27d4fa2465232893eae1b3669b47
kamga98 <smkamga@gmail.com>
2025-10-11 01:08:37 +0200

    Redirection du client sur la page MY ACCOUNT après un paiement

--- src/Controller/MollieController.php
+++ src/Controller/MollieController.php
@@ -12,81 +12,95 @@ use Mollie\Api\MollieApiClient;
 
 final class MollieController extends AbstractController
 {
+      
   
-    private MollieApiClient $mollie;
+    // private MollieApiClient $mollie;
   
-    public function __construct()
-    {  
-        $this->mollie = new MollieApiClient();
+    // public function __construct()
+    // {  
+    //     $this->mollie = new MollieApiClient();
  
-      // $api_key = getenv('MOLLIE_KEY'); 
-      // dd("API KEY =>", $api_key);            
-      //  $this->mollie->setApiKey(getenv('MOLLIE_KEY'));
+    //   // $api_key = getenv('MOLLIE_KEY'); 
+    //   // dd("API KEY =>", $api_key);            
+    //   //  $this->mollie->setApiKey(getenv('MOLLIE_KEY'));
         
-      /* We put directly "test_rmhBV2hB2edSbfnuE59HNubSTjVMEE" just for a test.  
-        Normally, it must be getenv('MOLLIE_KEY') 
-      */
-       $this->mollie->setApiKey("test_rmhBV2hB2edSbfnuE59HNubSTjVMEE");
-         
-    }      
+    //   /* We put directly "test_rmhBV2hB2edSbfnuE59HNubSTjVMEE" just for a test.  
+    //     Normally, it must be getenv('MOLLIE_KEY') 
+    //   */
+    //    // $this->mollie->setApiKey("test_rmhBV2hB2edSbfnuE59HNubSTjVMEE");
+    //    $this->mollie->setApiKey("live_8BKesqNy2rfyGSD5RqVR2W44yqkTtz");   
+           
+    // }      
       
       
-    // Crée un paiement et redirige l’utilisateur vers Mollie
-    // Le nom "SANTIAGO MARTINEZ KAMGA" est indiqué en guise de titre du formulaire car
-    // le marchand est le titulaire du compte Mollie : SANTIAGO MARTINEZ KAMGA dans notre cas.
-    // Ce dernier est lié au formulaire via la clé api : test_rmhBV2hB2edSbfnuE59HNubSTjVMEE 
-    #[Route('/test_with_mollie', name: 'mollie_test')]
-    public function preparePayment()
-    {      
+    // // Crée un paiement et redirige l’utilisateur vers Mollie
+    // // Le nom "SANTIAGO MARTINEZ KAMGA" est indiqué en guise de titre du formulaire car
+    // // le marchand est le titulaire du compte Mollie : SANTIAGO MARTINEZ KAMGA dans notre cas.
+    // // Ce dernier est lié au formulaire via la clé api : test_rmhBV2hB2edSbfnuE59HNubSTjVMEE 
+    // #[Route('/test_with_mollie', name: 'mollie_test')]
+    // public function preparePayment()
+    // {      
         
-        /* La propriété "paymeny_success" de redirectUrl permet d'éxécuter le code de la fonction
-        nommée payment_success.  
-        */  
-        $payment = $this->mollie->payments->create([
-            "amount" => [
-                "currency" => "EUR",
-                "value" => "10.00"
-            ],
-            "description" => "Test payment with Mollie",
-            "redirectUrl" => $this->generateUrl('payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
-        ]);  
+    //     /* La propriété "paymeny_success" de redirectUrl permet d'éxécuter le code de la fonction
+    //     nommée payment_success.  
+    //     */  
+    //     // $payment = $this->mollie->payments->create([
+    //     //     "amount" => [
+    //     //         "currency" => "EUR",
+    //     //         "value" => "10.00"
+    //     //     ],
+    //     //     "description" => "Test payment with Mollie",
+    //     //     "redirectUrl" => $this->generateUrl('payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+    //     // ]);       
+
+    //     $payment = $this->mollie->payments->create([
+    //         "amount" => [
+    //             "currency" => "EUR",
+    //             "value" => "10.00"
+    //         ],
+    //         "description" => "Achat sur Sylius via Mollie",
+    //         "redirectUrl" => $this->generateUrl('payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+    //         "webhookUrl" => $this->generateUrl('mollie_webhook', [], UrlGeneratorInterface::ABSOLUTE_URL),
+    //     ]);
+
   
   
-        /* Le code de statut de réponse de redirection 303 See Other, 
-        généralement renvoyé comme résultat d'une opération PUT ou POST, 
-        indique que la redirection ne fait pas le lien vers la ressource 
-        nouvellement téléversée mais vers une autre page (par exemple une page 
-        de confirmation ou qui affiche l'avancement du téléversement). 
-        La méthode utilisée pour afficher la page redirigée est toujours GET.
-        Dans notre cas, la page affichée est la page de paiement.   
-        */     
-        return new RedirectResponse($payment->getCheckoutUrl(), 303);
+    //     /* Le code de statut de réponse de redirection 303 See Other, 
+    //     généralement renvoyé comme résultat d'une opération PUT ou POST, 
+    //     indique que la redirection ne fait pas le lien vers la ressource 
+    //     nouvellement téléversée mais vers une autre page (par exemple une page 
+    //     de confirmation ou qui affiche l'avancement du téléversement). 
+    //     La méthode utilisée pour afficher la page redirigée est toujours GET.
+    //     Dans notre cas, la page affichée est la page de paiement.   
+    //     */     
+    //     return new RedirectResponse($payment->getCheckoutUrl(), 303);
  
-    }
+    // }
   
    
-    // Callback de succès (après paiement)
-    #[Route('/payment-success', name: 'payment_success')]
-    public function paymentSuccess(Request $request)
-    {
+    // // Callback de succès (après paiement)
+    // #[Route('/payment-success', name: 'payment_success')]
+    // public function paymentSuccess(Request $request)
+    // {
        
-        dd("Payment successful!");    
+    //     dd("Payment successful!");    
 
-        // Normally we have to go back the homepage of the sylius_shop_user  
-        return new RedirectResponse("Payment successful!"); 
+    //     // Normally we have to go back the homepage of the sylius_shop_user  
+    //     return new RedirectResponse("Payment successful!"); 
 
-    }
-
+    // }
+  
+   
 
 
     
-    #[Route('/mollie', name: 'app_mollie')]
-    public function index(): Response
-    {
-        return $this->render('mollie/index.html.twig', [
-            'controller_name' => 'MollieController',
-        ]);
-    }
+    // #[Route('/mollie', name: 'app_mollie')]
+    // public function index(): Response
+    // {
+    //     return $this->render('mollie/index.html.twig', [
+    //         'controller_name' => 'MollieController',
+    //     ]);
+    // }
    
 }
 
--- src/Controller/PaymentRedirectController.php
+++ src/Controller/PaymentRedirectController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;      
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\Request;   
+use Symfony\Component\HttpFoundation\Request;     
 use Mollie\Api\MollieApiClient;   
    
    
@@ -22,27 +22,25 @@ final class PaymentRedirectController extends AbstractController
        $this->mollie = new MollieApiClient();
  
        $this->mollie->setApiKey("test_rmhBV2hB2edSbfnuE59HNubSTjVMEE");
-         
-    }      
-      
-
-    #[Route('/payment/redirect', name: 'app_payment_redirect')]
-    public function index(): Response
-    {
-        return $this->render('payment_redirect/index.html.twig', [
-            'controller_name' => 'PaymentRedirectController',
-        ]);
-    }
-
+       // $this->mollie->setApiKey("live_8BKesqNy2rfyGSD5RqVR2W44yqkTtz");
+           
+    }         
+       
        
     /* Lorsque l'utilisateur est sur la page dont l'adresse url est http://127.0.0.1:8000/en_US/cart/
     et qu'il clique sur le bouton "Next", cette fonction est appelée.
     Le client pourra transmettre son adresse postale par courriel au marchand et ce dernier
     mettra à jour cette information manuellement dans la base de données, s'il désire la stocker. 
-    Il pourra ainsi la transmettre à l'agence de livraison de la commande.                  
+    Il pourra ainsi la transmettre à l'agence de livraison de la commande.  
+    Via le dashboard de Mollie, nous parvenons déjà à voir le changement du statut d'un paiement
+    qui passe de 'ouvert' à 'payé'.  
+
+    Question fondamentale : L'utilisateur est-il vraiment facturé ? 
+    Comment savoir si je n'ai utilisé ma carte bancaire ?   
+                        
     */      
      #[Route('/{_locale}/checkout/address', name: 'sylius_shop_checkout_address', methods: ['GET', 'PUT'])]
-    public function redirectToMollie(Request $request): RedirectResponse
+    public function redirectToMollie(): RedirectResponse
     {       
                       
         /* Nous devrons faire en sorte que les données de la variable $payment 
@@ -50,32 +48,75 @@ final class PaymentRedirectController extends AbstractController
          $payment = $this->mollie->payments->create([
             "amount" => [
                 "currency" => "EUR",
-                "value" => "10.00"
+                "value" => "10.00" 
             ],
             "description" => "Test payment with Mollie",
             "redirectUrl" => $this->generateUrl('payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
-        ]);  
-
+          //  "webhookUrl" => $this->generateUrl('mollie_webhook', [], UrlGeneratorInterface::ABSOLUTE_URL),
+                 
+        ]);   
+   
           
         // Peut-on récupérer les données de la requête pour les transmettre au formulaire ?
-        // dd("request ===>", $request);    
-
+         
         // Redirige l'utilisateur vers Mollie. 
         return new RedirectResponse($payment->getCheckoutUrl(), 303);
-   
-
-   
+           
+     
         /* Résultat attendu : 
         - Le client mentionne son adresse. 
         - Il clique sur Next.
         - Sylius passe normalement à /en_US/checkout/address mais notre surchage (notre contrôleur)
         intercepte la requête. 
-        - Le client est redirigé automatiquement vers Mollie. 
-
+        - Le client est redirigé automatiquement vers Mollie.  
         */ 
           
      }
+
+    
+    #[Route('/mollie/webhook', name: 'mollie_webhook', methods: ['POST'])]
+    public function mollieWebhook(Request $request): Response
+    {  
   
+        dd('Execution de la methode mollieWebhook');   
+        
+        $paymentId = $request->request->get('id'); // Mollie envoie "id" du paiement
+        $payment = $this->mollie->payments->get($paymentId);
+
+        if ($payment->isPaid()) {
+            // 💾 Mets à jour ton paiement / commande en base
+            // ex: $order->setStatus('paid'); $em->flush();
+            dd('id du paiement', $paymentId);     
+        }
+   
+        // Réponds toujours un 200 à Mollie pour éviter les retries
+        return new Response('OK', 200);
+    }
+
+
+    // Callback de succès (après paiement)
+    #[Route('/payment-success', name: 'payment_success')]
+    public function paymentSuccess(Request $request)
+    {
+         
+       // dd("request ===>", $request);  
+       // dd("Payment successful!");            
+       return $this->redirectToRoute('sylius_shop_account_dashboard');
+  
+        // Normally we have to go back the homepage of the sylius_shop_user  
+       return new RedirectResponse("Payment successful!"); 
+
+    }
+
+    
+
+    #[Route('/payment/redirect', name: 'app_payment_redirect')]
+    public function index(): Response
+    {
+        return $this->render('payment_redirect/index.html.twig', [
+            'controller_name' => 'PaymentRedirectController',
+        ]);
+    }  
       
 }
 
